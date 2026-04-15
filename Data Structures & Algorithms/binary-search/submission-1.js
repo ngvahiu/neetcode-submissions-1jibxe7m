3465a35876c6b9c7fb48da75113bb3724fa84167
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-04-15 16:01:48 +0700

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0, right = nums.length - 1;
+        while (left <= right) {
+            let mid = left + Math.floor((right - left) / 2);
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        
+        return -1;
+    }
+}
