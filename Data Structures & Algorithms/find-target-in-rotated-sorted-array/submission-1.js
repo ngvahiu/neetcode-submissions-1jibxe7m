7cb0241d1c3e07c1ddf17e66594c0c08d5038cb9
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-04-18 13:27:13 +0700

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            const m = l + Math.floor((r - l) / 2);
+
+            if(nums[m] === target) return m;
+
+            if (nums[m] < nums[r]) {
+                // right sorted
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                // left sorted
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
