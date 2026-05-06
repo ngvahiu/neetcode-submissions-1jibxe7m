830078ddb373b25080949fddc8757f601e4afd7f
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-05-06 22:33:37 +0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        const dp = new Array(n + 1).fill(0);
+
+        dp[0] = 1;
+        for (let i = 1; i <= n; i++) {
+            dp[i] += dp[i - 1];
+            if (i - 2 >= 0) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
