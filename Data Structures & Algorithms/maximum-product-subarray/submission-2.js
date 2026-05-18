13baa661d996d7bec31052de698b8dcef9d12ba8
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-05-18 22:07:04 +0700

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxProduct(nums) {
+        let res = nums[0];
+        let curMin = nums[0];
+        let curMax = nums[0];
+
+        for (let i = 1; i < nums.length; i++) {
+            let n = nums[i];
+            if(n < 0) {
+                [curMax, curMin] = [curMin, curMax];
+            }
+
+            curMax = Math.max(curMax * n, n);
+            curMin = Math.min(curMin * n, n);
+
+            res = Math.max(curMax, res);
+        }
+
+        return res;
+    }
+}
