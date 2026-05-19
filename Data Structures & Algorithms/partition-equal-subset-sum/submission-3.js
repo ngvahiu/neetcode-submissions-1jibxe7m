98c42a2341f8953bb13cca08b941fd601b43b558
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-05-19 09:51:57 +0700

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canPartition(nums) {
+        const totalSum = nums.reduce((acc, curr) => acc + curr, 0);
+        if(totalSum % 2 === 1) return false;
+        
+        const target = totalSum / 2;
+        const memo = Array.from({ length: nums.length }, () => Array.from({ length: target + 1 }, () => null));
+        function dp(i, remain) {
+            if(i < 0) return false;
+            const n = nums[i];
+
+            if(n === remain) return true;
+            if(memo[i][remain] !== null) return memo[i][remain];
+            if(n > remain) return dp(i - 1, remain);
+
+            // choose n
+            if(dp(i - 1, remain - n)) {
+                memo[i][remain] = true;
+                return true;
+            } else {
+                memo[i][remain] = false;
+            }
+
+            // without choosing n
+            return  dp(i - 1, remain);
+        }
+
+        return dp(nums.length - 1, target);
+    }
+}
