ee52a94630a71e3fc2b66d87ae38719a94587633
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-05-20 22:47:15 +0700

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+        const dp = Array.from({ length: m }, () => Array(n).fill(0));
+
+        dp[0][0] = 1;
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                if (i - 1 >= 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if (j - 1 >= 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
