dc71968499ba1340a8dc4ae43e84563b4924f925
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-05-21 23:07:46 +0700

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} amount
+     * @param {number[]} coins
+     * @return {number}
+     */
+    change(amount, coins) {
+        const dp = new Array(amount + 1).fill(0);
+
+        dp[0] = 1;
+
+        for (const coin of coins) {
+            for (let i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[amount];
+    }
+}
