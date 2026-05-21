ad0c8e056aa0a0efebf08ae8ede5bba925906944
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-05-21 23:49:42 +0700

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    findTargetSumWays(nums, target) {
+        const dp = {};
+
+        function dfs(i, curSum) {
+            if (i >= nums.length) {
+                return curSum === target ? 1 : 0;
+            }
+            let key = `${i}-${curSum}`;
+            if (key in dp) {
+                return dp[key];
+            }
+
+            let pos = dfs(i + 1, curSum + nums[i]);
+            let neg = dfs(i + 1, curSum - nums[i]);
+            dp[key] = pos + neg;
+            return dp[key];
+        }
+
+        return dfs(0, 0);
+    }
+}
