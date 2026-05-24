e8122c86873109cb045a385273324aa4a5beb563
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-05-24 17:05:04 +0700

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} gas
+     * @param {number[]} cost
+     * @return {number}
+     */
+    canCompleteCircuit(gas, cost) {
+        const totalGas = gas.reduce((acc, cur) => acc + cur, 0);
+        const totalCost = cost.reduce((acc, cur) => acc + cur, 0);
+
+        if (totalCost > totalGas) return -1;
+
+        let res = 0;
+        let total = 0;
+        for (let i = 0; i < gas.length; i++) {
+            total += (gas[i] - cost[i]);
+            if (total < 0) {
+                total = 0;
+                res = i + 1 >= gas.length ? 0 : i + 1;
+            }
+        }
+
+        return res;
+    }
+}
