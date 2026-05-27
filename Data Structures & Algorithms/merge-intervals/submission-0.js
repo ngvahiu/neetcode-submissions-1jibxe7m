ce54a1d718cc20f288f575471cf26f0180f3c394
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-05-27 21:46:45 +0700

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a, b) => {
+            if (a[0] !== b[0]) return a[0] - b[0];
+            return a[1] - b[1];
+        });
+
+        const res = [];
+        let curInterval = intervals[0];
+
+        for (let i = 0; i < intervals.length; i++) {
+            if (curInterval[1] < intervals[i][0]) {
+                res.push(curInterval);
+                curInterval = intervals[i];
+            } else {
+                curInterval[1] = Math.max(curInterval[1], intervals[i][1]);
+            }
+        }
+
+        res.push(curInterval);
+        return res;
+    }
+}
