183941a561ee288f59bd357003dee6867ed6ff3b
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-05-31 22:01:25 +0700

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums) {
+        let res = 0;
+        for (let i = 0; i <= nums.length; i++) {
+            res ^= i;
+        }
+
+        res ^= nums.reduce((acc, cur) => acc ^ cur, 0);
+        return res;
+    }
+}
