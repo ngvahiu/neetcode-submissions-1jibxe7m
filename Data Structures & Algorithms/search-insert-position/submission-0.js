fed48f9fff75b6b25e4f79a3add4f5cf899567d1
Hieu Nguyen <93477565+ngvahiu@users.noreply.github.com>
2026-07-27 01:32:26 +0700

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums, target) {
+        let l = 0,
+            r = nums.length - 1;
+
+        while (l <= r) {
+            const m = Math.floor((l + r) / 2);
+
+            if (nums[m] < target) {
+                l = m + 1;
+            } else if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                return m;
+            }
+        }
+
+        return l;
+    }
+}
